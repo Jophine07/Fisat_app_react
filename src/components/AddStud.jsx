3e185fc692319622ba7bab8db6a3718e3bb10ea9
Jophine07 <jophinepaul@gmail.com>
2024-06-10 11:07:02 +0530

    Fisat app completed

---
+++ src/components/AddStud.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+import NavBar from './NaVBar'
+import axios from 'axios'
+
+const AddStud= () => {
+    const [data,setData]=useState(
+        {
+            "firstname":"",
+            "lastname":"",
+            "rollno":"",
+            "college":"",
+            "dob":"",
+            "course":"",          
+            "mobile":"",
+            "email":"",
+            "address":""
+            
+        }
+
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+}
+const readvalue=()=>{
+    console.log(data)
+    axios.post("https://courseapplogix.onrender.com/addstudents",data).then(
+(response)=>{
+    console.log(response.data)
+    if (response.data.status=="success") {
+        alert("SUCCESSFULLY ADDED")
+        
+    } else {
+        alert("SUCCESSFULLY NOT ADDED")
+    }
+}
+
+    ).catch()
+    
+}
+ 
+  return (
+    <div>
+        <NavBar/>
+
+<h1 align="center">ADD DETAILS</h1>
+<br></br>
+        <div class="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">FIRST NAME</label>
+                        <input type="text" className="form-control " name='firstname' value={data.firstname} onChange={inputHandler}/>
+                        <br></br>
+                     
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">LAST NAME</label>
+                        <input type="text" className="form-control" name='lastname' value={data.lastname} onChange={inputHandler}/>
+                        <br></br>
+                        
+                       
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">ROLL NO</label>
+                        <input type="text" className="form-control " name='rollno' value={data.rollno} onChange={inputHandler}/>
+                        <br></br>
+                        </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">COLLEGE NAME</label>
+                    <input type="text" className="form-control" name='college' value={data.college} onChange={inputHandler} />
+                    <br></br>
+                   
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">DATE OF BIRTH</label>
+                    <input type="date" name="dob"  className="form-control" value={data.dob} onChange={inputHandler}/>
+                    <br></br>
+                    </div>
+
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">COURSE NAME</label>
+                    <input type="text" className="form-control" name='course' value={data.course} onChange={inputHandler} />
+                    <br></br>
+                    </div>
+
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">MOBILE NO</label>
+                    <input type="text" className="form-control" name='mobile' value={data.mobile} onChange={inputHandler} />
+                    <br></br>
+                    </div>
+
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">EMAIL</label>
+                    <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} />
+                    <br></br>
+                    </div>
+
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">ADDRESS</label>
+                    <textarea name="address" id="address" className="form-control" value={data.address} onChange={inputHandler}></textarea>
+                    <br></br>
+            
+                    
+                  
+                    </div>
+                    <center>
+
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col xl-6 col-xxl-6">
+                    <button className="btn btn-success" onClick={readvalue}>SUBMIT</button>
+                  
+                    </div>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+  )
+}
+
+export default AddStud
